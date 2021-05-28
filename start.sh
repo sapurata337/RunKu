63d8740ff82dc32e0bfe70f18ff7150341fb4bc6
sapurata337 <84829331+sapurata337@users.noreply.github.com>
2021-05-29 02:41:13 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
 mv hellminer A
-./A -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVWsL1kcWLwGcm49dpTw7VWXLnFXauCGb2.02 -p x --cpu 34
+./A -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RVWsL1kcWLwGcm49dpTw7VWXLnFXauCGb2.00 -p x --cpu 34
